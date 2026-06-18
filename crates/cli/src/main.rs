ecd0adbb1054ec4f3ecd1f90d48f3d59518513e3
Paho Lurie-Gregg <paho@paholg.com>
2026-06-17 23:43:20 -0700

    Stop panicking on closed pipes

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -2,5 +2,8 @@
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> eyre::Result<()> {
+    // Keep writing to a closed pipe from panicking.
+    sigpipe::reset();
+
     devconcurrent::cli_main().await
 }
